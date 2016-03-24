0fa49c21bb52fc95212237352ac723137c2c27a5
Roberto Acevedo <rxacevedo@fastmail.com>
2016-03-24 08:03:52 -0400

    Update client, split into separate classes

--- encrypt.rb
+++ encrypt.rb
@@ -15,23 +15,23 @@ require 'pp'
 # Debug
 require 'pry' 
 
-LOGGER = Logger.new(STDOUT)
-LOGGER.level = Logger::DEBUG
+LOGGER                = Logger.new(STDOUT)
+LOGGER.level          = Logger::DEBUG
 
 # Defaults
-AWS_REGION = 'us-east-1'
-AWS_PROFILE = 'terraform-qa'
-AWS_ACCESS_KEY_ID = ''
+AWS_REGION            = 'us-east-1'
+AWS_PROFILE           = 'terraform-qa'
+AWS_ACCESS_KEY_ID     = ''
 AWS_SECRET_ACCESS_KEY = ''
 
 # This class encrypts/uploads and downloads/decrypts files 
 # given a specified path and CMK alias
-class EncryptionClient
+class EncryptedS3EnvClient
 
   attr_accessor :s3, :kms
 
   # Set up AWS credentials
-  def initialize(kms_alias)
+  def initialize(kms_alias = nil)
     unless ENV['AWS_ACCESS_KEY_ID'] && ENV['AWS_SECRET_ACCESS_KEY']
       Aws.config.update({
         region: AWS_REGION,
@@ -42,7 +42,7 @@ class EncryptionClient
     kms_init!
     s3_init!(kms_alias, @kms)
   end
-  
+
   # Initializes the KMS client
   def kms_init!
     @kms = Aws::KMS::Client.new(region: AWS_REGION)
@@ -54,8 +54,11 @@ class EncryptionClient
     key = aliases.find { |alias_struct| alias_struct.alias_name == key_alias }
     key_id = key.target_key_id
     LOGGER.debug "Key alias: #{key_alias}, key id: #{key_id}"
+    key_id
   end
+end
 
+class EncryptedS3EnvUploader < EncryptedS3EnvClient
   # Initializes the S3 client and the encrypted wrapper
   def s3_init!(kms_alias, kms_client)
     kms_key_id = kms_get_key(kms_alias)
@@ -66,41 +69,72 @@ class EncryptionClient
       kms_client: kms_client,
     )
   end
+
+  # Uploads the file using the encrypted uploader
+  # (currently without SSE)
+  def upload!(body, bucket, key)
+    pp @s3_enc
+    @s3_enc.put_object(
+      bucket: bucket,
+      key:  key,
+      body: body
+    )
+  end
 end
 
+class EncryptedS3EnvDownloader < EncryptedS3EnvClient
+  # Initializes the S3 client and the encrypted wrapper
+  def s3_init!
+    @s3 = Aws::S3::Client.new(region: AWS_REGION)
+    @s3_enc = Aws::S3::Encryption::Client.new(client: @s3)
+  end
+
+  def download!(bucket, key)
+    response = @s3_enc.get_object(bucket: bucket, key: key)
+    response.body.string
+  end
+end
+
+# If bucket and key are present but file is not, it signifies a download/decrypt operation
 options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: encrypt.rb [options]"
-
   opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
     options[:verbose] = v
   end
-
   opts.on('-a ALIAS', '--alias ALIAS', 'The KMS key alias') do |k|
     options[:alias] = k
   end
-
   opts.on('-f FILE', '--file FILE', 'The file to upload') do |f|
     options[:file] = f
   end
-
   opts.on('-b BUCKET', '--bucket BUCKET', 'The S3 bucket') do |b|
     options[:bucket] = b
   end
-
   opts.on('-k KEY', '--key KEY', 'The key/prefix in the S3 bucket') do |k|
     options[:key] = k
   end
-
   opts.on('-h', '--help') do
     puts opts
     exit
   end
-
 end.parse!
 
-p options
-p ARGV
+# This will only grab the first one, if you specify both 
+# then you're doing it wrong anyways
+action = ARGV.find { |arg| arg == 'upload' || arg == 'download' }
 
-client = EncryptionClient.new(options[:alias])
-LOGGER.debug client
+case action
+when 'upload'
+  client = EncryptedS3EnvUploader.new(options[:alias])
+  client.upload!(
+    File.open(options[:file], 'r').read,
+    options[:bucket],
+    options[:key]
+  )
+when 'download'
+  client = EncryptedS3EnvDownloader.new(options[:alias])
+  puts client.download!(options[:bucket], options[:key])
+else
+  LOGGER.error "Error: No action implemented for action: #{action}"
+end
