5c159098c98eecfd8e0dede2db71e4e1a854c6d3
Roberto Acevedo <rxacevedo@fastmail.com>
2016-03-30 10:14:58 -0400

    Implement upload/download from the CLI

--- lib/secretshelper/s3.rb
+++ lib/secretshelper/s3.rb
@@ -2,22 +2,35 @@ require 'thor'
 
 module SecretsHelper
   class S3 < Thor
-    # - If cp is copying *from S3 to local*, then --decrypt can mean "decrypt the file before writing to disk"
-    # - If cp is copying *from local to s3*, then --encrypt can mean "encrypt the file before writing to S3"
     # TODO: This still doesn't account for the S3EncryptedClient's upload/download functions, which read/write binary
     # encoded files (not the JSON/base64 stuff that I'm doing here).
-    # Options:
-    # - cp <SRC> <DEST> (parse paths to determine if S3 bucket/key prefix vs local directory
     desc 'cp', 'Use the S3 API to copy files'
     method_option :encrypt, :type => :boolean
     method_option :decrypt, :type => :boolean
     def cp (*args)
-    end
+      src, dest =  args.take 2
+      src_type, dest_type = [S3Helper.parse_path(src), S3Helper.parse_path(dest)]
 
-    # no_commands do
-    #   def parse_paths(*paths)
-    #   end
-    # end
+      event = if src_type == PathTypes::UNIX && dest_type == PathTypes::S3
+                :upload
+              elsif src_type == PathTypes::S3 && dest_type == PathTypes::UNIX
+                :download
+              else
+                :invalid
+              end
+
+      if event == :upload
+        bucket, key = S3Helper.parse_s3_path dest
+        client = S3Helper.new(options[:encrypt])
+        client.upload(bucket, key, File.open(src, 'r').read)
+      elsif event == :download
+        bucket, key = S3Helper.parse_s3_path src
+        client = S3Helper.new(options[:decrypt])
+        client.download(bucket, key, dest)
+      else puts "Invalid"
+      end
+
+    end
 
   end
 
@@ -31,6 +44,8 @@ module SecretsHelper
 
   # S3 wrapper class
   class S3Helper
+    S3_PATH_REGEX = /s3:\/\/(.*?)\/(.*)/
+
     def initialize(kms_key_id = nil)
       @s3 = Aws::S3::Client.new(region: SecretsHelper::Const::AWS_REGION) unless kms_key_id
       @s3 = Aws::S3::Encryption::Client.new(
@@ -39,7 +54,7 @@ module SecretsHelper
     end
 
     # Performs a PutObject operation for content at s3://bucket/key
-    def upload!(bucket, key, body)
+    def upload(bucket, key, body)
       @s3.put_object(
         bucket: bucket,
         key:  key,
@@ -56,17 +71,16 @@ module SecretsHelper
       puts content unless dest
     end
 
-    # TODO: It would probably be a good idea to move this logic
-    # into a module since it has no need for the S3 client itself.
-    # Maybe a good candidate for a mixin
-    # Determines whether a path is a filesystem or S3 path. 
-    def parse_path(path)
+    def self.parse_path(path)
       # If we "parse" it as an S3 path, we'll let the S3 client throw
       # an error if it's a non-existant path
-      if path.start_with? 's3://' then PathTypes::S3
-      elsif File.exists? path then PathTypes::UNIX
-      else raise "Invalid path: #{path}"
-      end
+      return PathTypes::S3 if path.start_with? 's3://'
+      return PathTypes::UNIX unless path.start_with? 's3://'
+    end
+
+    def self.parse_s3_path(s3_path)
+      bucket, key = s3_path.match(S3_PATH_REGEX).captures
+      [bucket, key]
     end
   end
 
