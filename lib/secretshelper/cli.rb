930a69cd1683e13c6a328f48620f34658cd1da37
Roberto Acevedo <rxacevedo@fastmail.com>
2016-04-01 17:26:49 -0400

    Add simplified local decyption logic

--- lib/secretshelper/cli.rb
+++ lib/secretshelper/cli.rb
@@ -35,38 +35,25 @@ module SecretsHelper
     method_option :file, :type => :string
     method_option :env_formatted, :type => :boolean
     method_option :out, :type => :string
+    method_option :key, :type => :string, :required => true
     def decrypt(*args)
 
-      # # PHASES:
-      # # - Initialize client
-      # # - HANDLE INPUT
-      # # - ENCRYPT
-      # # - HANDLE OUTPUT
 
-      # client = SecretsHelper::KMS::DecryptionHelper.new
+      key = Base64.strict_decode64(options[:key])
+      client = SecretsHelper::Crypto::DecryptionHelper.new(key)
 
-      # if options[:file]
-      #   # Decrypt file that was encrypted
-      #   # by client
-      #   file = File.open(options[:file], 'r')
+      if options[:file]
+        file = File.open(options[:file], 'r')
+        plaintext = client.decrypt(file.read) unless options[:env_formatted]
+        plaintext = client.decrypt_env_file(file.read) if options[:env_formatted]
+        handle_output(plaintext)
+      else
+        data = handle_stdin
+        plaintext = client.decrypt(data) unless options[:env_formatted]
+        plaintext = client.decrypt_env_file(data) if options[:env_formatted]
+        handle_output(plaintext)
+      end
 
-      #   # TODO: This kind of control-flow feels weird.
-      #   plaintext = client.decrypt(file.read) unless options[:env_formatted]
-      #   plaintext = client.decrypt_env_file(file.read) if options[:env_formatted]
-
-      #   # Output in both cases
-      #   puts plaintext unless options[:out]
-      #   File.open(options[:out], 'w') { |f| f.write plaintext } if options[:out]
-      # else
-      #   data = ''
-      #   data << $LAST_READ_LINE while $stdin.gets
-      #   plaintext = client.decrypt(data) unless options[:env_formatted]
-      #   plaintext = client.decrypt_env_file(data) if options[:env_formatted]
-
-      #   # STDOUT is assumed for STDIN input (no CLI
-      #   # --text input currently implemented)
-      #   # TODO: Don't assume STDOUT, check for --out flag
-      #   puts plaintext
     end
 
     desc 's3', 'Use the S3 API'
